14ede009dfeef3eacf6c1b53a9244042614a1a27
Chaitanya Jujjavarapu <chaitanya@wirelessprv-10-194-31-196.near.illinois.edu>
2017-12-11 19:51:53 -0600

    completed the python game of tic tac toe.

--- tictactoeactual.py
+++ tictactoeactual.py
@@ -16,7 +16,7 @@ def thedisplayboard(board):
     print('   |   |')
 
 
-def player_input():
+def userinput():
     point = ''
     while not (point == 'X' or point == 'O' or point == 'Quit'):
         point = raw_input('Player 1: Would you like to be X or O?').upper()
@@ -27,42 +27,42 @@ def player_input():
         return 'O', 'X'
 
 
-def place_point(board, point, position):
+def placepoint(board, point, position):
     board[position] = point
 
 
-def win_check(board, mark):
-    return((board[7] == mark and board[8] == mark and board[9] == mark) or
+def checkwin(board, mark):
+    return((board[1] == mark and board[2] == mark and board[3] == mark) or
     (board[4] == mark and board[5] == mark and board[6] == mark) or
-    (board[1] == mark and board[2] == mark and board[3] == mark) or
-    (board[7] == mark and board[4] == mark and board[1] == mark) or
-    (board[8] == mark and board[5] == mark and board[2] == mark) or
-    (board[9] == mark and board[6] == mark and board[3] == mark) or
-    (board[7] == mark and board[5] == mark and board[3] == mark) or
-    (board[9] == mark and board[5] == mark and board[1] == mark))
+    (board[7] == mark and board[8] == mark and board[9] == mark) or
+    (board[1] == mark and board[4] == mark and board[7] == mark) or
+    (board[2] == mark and board[5] == mark and board[8] == mark) or
+    (board[3] == mark and board[6] == mark and board[9] == mark) or
+    (board[3] == mark and board[5] == mark and board[7] == mark) or
+    (board[1] == mark and board[5] == mark and board[9] == mark))
 
 
-def chooseFirst():
+def choosefirst():
     if random.randint(0, 1) == 0:
         return 'Player 2'
     else:
         return 'Player 1'
 
 
-def checkSpace(board, position):
+def checkspace(board, position):
     return board[position] == ' '
 
 
-def checkmainBoard(board):
+def checkmainboard(board):
     for i in range(1, 10):
-        if checkSpace(board, i):
+        if checkspace(board, i):
             return False
     return True
 
 
-def playerChoice(board, point):
+def playerchoice(board, point):
     position = ' '
-    while position not in '1 2 3 4 5 6 7 8 9'.split() or not checkSpace(board, int(position)):
+    while position not in '1 2 3 4 5 6 7 8 9'.split() or not checkspace(board, int(position)):
         position = raw_input(point + ' choose your next position: (1-9) ')
     return int(position)
 
@@ -76,23 +76,23 @@ print('Welcome to Tic Tac Toe!')
 
 while True:
     mainBoard = [' '] * 10
-    player1_point, player2_point = player_input()
-    turn = chooseFirst()
+    player1_point, player2_point = userinput()
+    turn = choosefirst()
     print(turn + ' will go first.')
-    game_on = True
+    gameStarted = True
 
-    while game_on:
+    while gameStarted:
         if turn == 'Player 1':
             thedisplayboard(mainBoard)
-            position = playerChoice(mainBoard, turn)
-            place_point(mainBoard, player1_point, position)
+            position = playerchoice(mainBoard, turn)
+            placepoint(mainBoard, player1_point, position)
 
-            if win_check(mainBoard, player1_point):
+            if checkwin(mainBoard, player1_point):
                 thedisplayboard(mainBoard)
                 print('Great Job! Player 1, you have won the game!')
-                game_on = False
+                gameStarted = False
             else:
-                if checkmainBoard(mainBoard):
+                if checkmainboard(mainBoard):
                     thedisplayboard(mainBoard)
                     print('TIE GAME!')
                     break
@@ -101,15 +101,15 @@ while True:
 
         else:
             thedisplayboard(mainBoard)
-            position = playerChoice(mainBoard, turn)
-            place_point(mainBoard, player2_point, position)
+            position = playerchoice(mainBoard, turn)
+            placepoint(mainBoard, player2_point, position)
 
-            if win_check(mainBoard, player2_point):
+            if checkwin(mainBoard, player2_point):
                 thedisplayboard(mainBoard)
                 print('Great Job! Player 2, you have won the game!')
-                game_on = False
+                gameStarted = False
             else:
-                if checkmainBoard(mainBoard):
+                if checkmainboard(mainBoard):
                     thedisplayboard(mainBoard)
                     print('TIE GAME!')
                     break
@@ -118,5 +118,4 @@ while True:
 
     if not replay():
         break
-
 
